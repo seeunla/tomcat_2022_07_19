dca1fa0c7de276b5d7908c56a97ed767406ab494
seeunla <ksu98712@gmail.com>
2022-07-21 15:10:05 +0900

    구구단을 model2 방식으로 처리

--- src/main/java/com/ll/exam/GugudanServlet.java
+++ src/main/java/com/ll/exam/GugudanServlet.java
@@ -1,5 +1,6 @@
 package com.ll.exam;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -11,16 +12,19 @@ import java.io.IOException;
 @WebServlet("/gugudan")
 public class GugudanServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)  {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Rq rq = new Rq(req, resp);
 
         int dan = rq.getIntParam("dan", 0);
         int limit = rq.getIntParam("limit", 0);
 
-        rq.appendBody("<h1>%d단</h1>\n".formatted(dan));
+        // request에 정보를 담는다.
+        // 왜냐하면 나중에 gugudan2.jsp에서 해당 내용을 꺼낼 수 있기 때문에
+        req.setAttribute("dan",dan);
+        req.setAttribute("limit",limit);
 
-        for (int i = 1; i <= limit; i++) {
-            rq.appendBody("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
-        }
+        // gugudan2.jsp 에게 나머지 작업을 토스
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/guguda2.jsp");
+        requestDispatcher.forward(req, resp);
     }
 }
