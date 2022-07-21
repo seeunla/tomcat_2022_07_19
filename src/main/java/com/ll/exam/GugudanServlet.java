198aee571591ecc2867fb75bc22ff3b1741a7847
seeunla <ksu98712@gmail.com>
2022-07-21 14:16:24 +0900

    구구단 jsp

---
+++ src/main/java/com/ll/exam/GugudanServlet.java
@@ -0,0 +1,26 @@
+package com.ll.exam;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/gugudan")
+public class GugudanServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)  {
+        Rq rq = new Rq(req, resp);
+
+        int dan = rq.getIntParam("dan", 0);
+        int limit = rq.getIntParam("limit", 0);
+
+        rq.appendBody("<h1>%d단</h1>\n".formatted(dan));
+
+        for (int i = 1; i <= limit; i++) {
+            rq.appendBody("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
+        }
+    }
+}
--- src/main/java/com/ll/exam/HelloServlet.java
+++ src/main/java/com/ll/exam/HelloServlet.java
@@ -13,11 +13,10 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        req.setCharacterEncoding("UTF-8");
-        resp.setCharacterEncoding("UTF-8");
+        req.setCharacterEncoding("UTF-8"); // 들어오는 데이터를 UTF-8로 해석하겠다.
+        resp.setCharacterEncoding("UTF-8"); // 완성되는 HTML의 인코딩을 UTF-8로 하겠다.
+        resp.setContentType("text/html; charset=utf-8"); // 브라우저에게 우리가 만든 결과물이 UTF-8이다 라고 말하는 의미
 
-        resp.setContentType("text/html; charset=utf-8");
-
-        resp.getWriter().append("안녕하세요!!!");
+        resp.getWriter().append("4444444444");
     }
 }
